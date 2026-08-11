a23668a521c97558469a7bd85268ec652f21cdea
dhumalharshada <144825296+dhumalharshada@users.noreply.github.com>
2026-08-11 14:22:02 +0530

    Update SwaggerConfig.java

--- car-rental-system-backend-master/car-rental-system-backend-master/src/main/java/com/carrentalsystem/config/SwaggerConfig.java
+++ car-rental-system-backend-master/car-rental-system-backend-master/src/main/java/com/carrentalsystem/config/SwaggerConfig.java
@@ -3,28 +3,35 @@ package com.carrentalsystem.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
 
-	// for swagger UI use --> http://localhost:8080/swagger-ui/index.html
+ // Access Swagger UI at: http://localhost:8080/swagger-ui/index.html
 
-	@Bean
-	public OpenAPI springShopOpenAPI() {
-		return new OpenAPI()
-				.info(new Info().title("Car Rental System Application")
-				.description("Car Rental Application using Spring Boot 3")
-				.version("v0.0.1")
-				.license(new License()
-				.name("Apache 2.0")
-				.url("http://springdoc.org")))
-				.externalDocs(new ExternalDocumentation().description("Code With Murad")
-				.url("https://codewithmurad.com"));
-	}
-	
-	
+ @Bean
+ public OpenAPI carRenatalOpenAPI() {
+  final String securitySchemeName = "bearerAuth";
+
+  return new OpenAPI()
+    .info(new Info()
+      .title("Car Rental System Application")
+      .description("Car Rental Application using Spring Boot 3")
+      .version("v0.0.1")
+      .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0")))
+    .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+    .components(new Components()
+      .addSecuritySchemes(securitySchemeName,
+        new SecurityScheme()
+          .name(securitySchemeName)
+          .type(SecurityScheme.Type.HTTP)
+          .scheme("bearer")
+          .bearerFormat("JWT")));
+ }
 }
